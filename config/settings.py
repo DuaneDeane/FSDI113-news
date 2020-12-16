3249701f8ffef02af56f94c88b3e8fa178fc8d23
Duane Deane <lilboom@Duanes-Air.attlocal.net>
2020-12-16 10:04:45 -0600

    trying to fix export env error

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'dtdeane123@gmail.com'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_SECRET = env.str("EMAIL_HOST_SECRET")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
